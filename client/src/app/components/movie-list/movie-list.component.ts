b4fc6fd41c4d7155575c34166fdb9025d8dee1b6
arreguy <brenoarreguy@gmail.com>
2023-05-17 07:31:59 -0300

    feat: edit and delete functionality on frontend

--- client/src/app/components/movie-list/movie-list.component.ts
+++ client/src/app/components/movie-list/movie-list.component.ts
@@ -19,4 +19,20 @@ export class MovieListComponent implements OnInit {
       error => console.error(error)
     );
   }
+
+  onEdit(movie: Movie): void {
+    const novoTitulo = prompt("Novo Título: ");
+    if (novoTitulo) {
+      const updatedMovie = {...movie, title: novoTitulo};
+      this.movieService.updateMovie(updatedMovie)
+      .subscribe();
+    }
+  }
+
+  onDelete(movie: Movie): void {
+    this.movieService.deleteMovie(movie)
+      .subscribe(deletedMovie => {
+        console.log("Filme deletado: ", deletedMovie);
+      });
+  }
 }
